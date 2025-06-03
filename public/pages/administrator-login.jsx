7e47a18cb2d5c1b135902ada34dc0e9d51b111ac
Carter Klauer <45862952+Carterklauer@users.noreply.github.com>
2025-06-03 10:26:09 -0500

    Update administrator-login.jsx

--- public/pages/administrator-login.jsx
+++ public/pages/administrator-login.jsx
@@ -1,1 +1,8 @@
-import react from React
+import * as React from "react"
+import {Link} from "wouter"
+import {UseEffect} from "react"
+
+export default function AdministratorLogin(){
+  return(
+    <h1 className="admin">Administrator Login</h1>
+  );
