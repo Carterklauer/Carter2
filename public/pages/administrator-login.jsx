87b33f15a876db7ef906a8fd396b83a7c21f0c89
Carter Klauer <45862952+Carterklauer@users.noreply.github.com>
2025-06-06 18:14:18 -0500

    Delete public/pages directory

--- public/pages/administrator-login.jsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react"
-import {Link} from "wouter"
-import {UseEffect} from "react"
-
-export default function AdministratorLogin(){
-  return(
-    <h1 className="admin">Administrator Login</h1>
-  );
